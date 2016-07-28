a740f304a81436e4c84f8212639202017a217095
surajit debnath <surajit.debnath@innoraft.com>
2016-07-28 14:27:36 +0530

    finish templating

--- sites/all/themes/own_theme/templates/page.tpl.php
+++ sites/all/themes/own_theme/templates/page.tpl.php
@@ -128,7 +128,7 @@
     </div>
 </div>
 
-<div>
+<div class="about_us_section">
     <div class="container">
          <div class="row">
             <div class="col-sm-12 about-us-tab animatedParent">
@@ -151,61 +151,60 @@
     </div>
 </div>
 
-<div class="main-container <?php print $container_class; ?>">
-
-  <header role="banner" id="page-header">
-    <?php if (!empty($site_slogan)): ?>
-      <p class="lead"><?php print $site_slogan; ?></p>
-    <?php endif; ?>
-
-    <?php print render($page['header']); ?>
-  </header> <!-- /#page-header -->
-    
-   
-
-  <div class="row">
-
-    <?php if (!empty($page['sidebar_first'])): ?>
-      <aside class="col-sm-3" role="complementary">
-        <?php print render($page['sidebar_first']); ?>
-      </aside>  <!-- /#sidebar-first -->
-    <?php endif; ?>
-
-    <section<?php print $content_column_class; ?>>
-      <?php if (!empty($page['highlighted'])): ?>
-        <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
-      <?php endif; ?>
-      <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
-      <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php if (!empty($title)): ?>
-        <h1 class="page-header"><?php print $title; ?></h1>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
-      <?php print $messages; ?>
-      <?php if (!empty($tabs)): ?>
-        <?php print render($tabs); ?>
-      <?php endif; ?>
-      <?php if (!empty($page['help'])): ?>
-        <?php print render($page['help']); ?>
-      <?php endif; ?>
-      <?php if (!empty($action_links)): ?>
-        <ul class="action-links"><?php print render($action_links); ?></ul>
-      <?php endif; ?>
-      <?php print render($page['content']); ?>
-    </section>
-
-    <?php if (!empty($page['sidebar_second'])): ?>
-      <aside class="col-sm-3" role="complementary">
-        <?php print render($page['sidebar_second']); ?>
-      </aside>  <!-- /#sidebar-second -->
-    <?php endif; ?>
-
-  </div>
+<div class="events">
+    <div class="container">
+        <div class="row animatedParent">
+            <?php print render($page['evetns']); ?>
+        </div>
+    </div>
+</div>
+<div class="testimonial">
+    <div class="container">
+        <div class="row animatedParent">
+            <?php print render($page['testimonial']); ?>
+        </div>
+    </div>
 </div>
 
+<div class="advantage">
+    <div class="container">
+        <div class="row animatedParent">
+            <?php print render($page['advantage']); ?>
+        </div>
+    </div>
+</div>
+
+<div class="team">
+    <div class="container">
+        <div class="row animatedParent">
+            <?php print render($page['team']); ?>
+        </div>
+    </div>
+</div>
+
+<div class="client-talk">
+    
+    <div class="container">
+        <div class="row animatedParent">
+            <?php print render($page['client_say']); ?>
+        </div>
+    </div>
+        
+</div>
+
+<div class="contact-us clearfix">
+    <div class="col-sm-6 map">
+        <img src="<?php print drupal_get_path('theme', 'own_theme') . '/images/contact.jpg'; ?>" alt="<?php print $site_name_and_slogan ?>" />
+    </div>
+    <div class="col-sm-6">
+        <h2>Contact</h2>
+        <?php print render($page['home_contact']); ?></div>          
+</div>
+
+
+
 <?php if (!empty($page['footer'])): ?>
-  <footer class="footer <?php print $container_class; ?>">
+  <footer class="footer <?php //print $container_class; ?>">
     <?php print render($page['footer']); ?>
   </footer>
 <?php endif; ?>
